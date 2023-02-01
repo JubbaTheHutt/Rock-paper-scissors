d479935e6b5d9a634144d0c4c95b3be25e7ecfd6
JubbaTheHutt <vladisabr@gmail.com>
2023-02-01 20:15:15 +0300

    some animations and favicon

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // let playerChoice = document.querySelectorAll(".players-choice .game-choice");
-// let computerChoice = document.querySelectorAll(
-//   ".computers-choice .game-choice"
-// );
+let computerChoice = document.querySelectorAll(
+  ".computers-choice .game-choice"
+);
 
 // function randomInteger() {
 //   let computerRandom = Math.floor(Math.random() * 3);
@@ -17,18 +17,24 @@ let loseCount = 0;
 rock.addEventListener("click", function () {
   weapon = "rock";
   computerRandom = Math.floor(Math.random() * 3);
+  toRight(rock);
+  toLeft(computerRandom);
   conditions();
 });
 
 scissors.addEventListener("click", function () {
   weapon = "scissors";
   computerRandom = Math.floor(Math.random() * 3);
+  toRight(scissors);
+  toLeft(computerRandom);
   conditions();
 });
 
 paper.addEventListener("click", function () {
   weapon = "paper";
   computerRandom = Math.floor(Math.random() * 3);
+  toRight(paper);
+  toLeft(computerRandom);
   conditions();
 });
 
@@ -58,5 +64,34 @@ function conditions() {
     alert("win");
     winCount++;
     player.innerHTML = winCount;
+  }
+}
+
+function toRight(name) {
+  name.style.transform = "translate(300px)";
+  setTimeout(() => (name.style.transform = "translate(0px)"), 2000);
+}
+
+function toLeft(num) {
+  if (num == 0) {
+    computerChoice[0].style.transform = "translate(-300px)";
+    setTimeout(
+      () => (computerChoice[0].style.transform = "translate(0px)"),
+      2000
+    );
+  }
+  if (num == 1) {
+    computerChoice[1].style.transform = "translate(-300px)";
+    setTimeout(
+      () => (computerChoice[1].style.transform = "translate(0px)"),
+      2000
+    );
+  }
+  if (num == 2) {
+    computerChoice[2].style.transform = "translate(-300px)";
+    setTimeout(
+      () => (computerChoice[2].style.transform = "translate(0px)"),
+      2000
+    );
   }
 }
